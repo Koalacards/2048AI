18c6c214bf01fd28062473a0208a2be8ea54fb23
Ben <ben@Bens-MacBook-Air.local>
2021-11-05 21:25:14 -0400

    comments and adjustable board size

--- main.py
+++ main.py
@@ -4,41 +4,64 @@ import sys
 import click
 
 CELL_WIDTH = 7
+CELL_HEIGHT = 3
+BASE_NUMBER = 2
+GRID_SIZE = 4
 
 class GameBoard():
     def __init__(self) -> None:
         self.score = 0
-        self.spaces = [[None] * 4 for i in range(4)]
+
+        # generate empty board
+        self.spaces = [[None] * GRID_SIZE for i in range(GRID_SIZE)]
+
+        # add two numbers to random spots on grid
         self.add_random_tile()
         self.add_random_tile()
 
     def __repr__(self):
-        out = '\u250C' + 3 * (CELL_WIDTH * '\u2500' + '\u252C') + CELL_WIDTH * '\u2500' + '\u2510\n'
-        out += self.space_lines()
+        # print top lines and first row of numbers/spaces
+        out = self.top_lines()
+        for i in range(math.ceil((CELL_HEIGHT - 1) / 2)):
+            out += self.space_lines()
         out += self.row_str(0)
-        out += self.space_lines()
-        out += '\u251C' + 3 * (CELL_WIDTH * '\u2500' + '\u253C') + CELL_WIDTH * '\u2500' + '\u2524\n'
-        out += self.space_lines()
-        out += self.row_str(1)
-        out += self.space_lines()
-        out += '\u251C' + 3 * (CELL_WIDTH * '\u2500' + '\u253C') + CELL_WIDTH * '\u2500' + '\u2524\n'
-        out += self.space_lines()
-        out += self.row_str(2)
-        out += self.space_lines()
-        out += '\u251C' + 3 * (CELL_WIDTH * '\u2500' + '\u253C') + CELL_WIDTH * '\u2500' + '\u2524\n'
-        out += self.space_lines()
-        out += self.row_str(3)
-        out += self.space_lines()
-        out += '\u2514' + 3 * (CELL_WIDTH * '\u2500' + '\u2534') + CELL_WIDTH * '\u2500' + '\u2518\n'
+        for i in range(math.floor((CELL_HEIGHT - 1) / 2)):
+            out += self.space_lines()
+
+        # print all grid lines and all remaining cells
+        for i in range(1, GRID_SIZE):
+            out += self.grid_lines()
+            for j in range(math.ceil((CELL_HEIGHT - 1) / 2)):
+                out += self.space_lines()
+            out += self.row_str(i)
+            for j in range(math.floor((CELL_HEIGHT - 1) / 2)):
+                out += self.space_lines()
+
+        # print bottom row of lines
+        out += self.bottom_lines()
+
+        # print current game score
         out += f"Score: {self.score}"
         return out
     
+    def top_lines(self):
+        return '\u250C' + (GRID_SIZE - 1) * (CELL_WIDTH * '\u2500' + '\u252C') + CELL_WIDTH * '\u2500' + '\u2510\n'
+
     def space_lines(self):
-        return 4 * ('\u2502' + CELL_WIDTH * ' ') + '\u2502\n'
+        return GRID_SIZE * ('\u2502' + CELL_WIDTH * ' ') + '\u2502\n'
+
+    def grid_lines(self):
+        return '\u251C' + (GRID_SIZE - 1) * (CELL_WIDTH * '\u2500' + '\u253C') + CELL_WIDTH * '\u2500' + '\u2524\n'
+
+    def bottom_lines(self):
+        return '\u2514' + (GRID_SIZE - 1) * (CELL_WIDTH * '\u2500' + '\u2534') + CELL_WIDTH * '\u2500' + '\u2518\n'
 
     def row_str(self, row):
+        """
+        Returns a string representing a line of the grid containing numbers.
+        """
         out = '\u2502'
-        for i in range(4):
+        for i in range(GRID_SIZE):
             cell = self.spaces[row][i]
             if cell is None:
                 cell = ' '
@@ -52,17 +75,25 @@ class GameBoard():
         return out
 
     def add_random_tile(self) -> None:
+        """
+        Adds a random tile to a blank space on the board.
+        """
         blank_spaces = []
         for i in range(len(self.spaces)):
             for j in range(len(self.spaces)):
                 if self.spaces[i][j] is None:
                     blank_spaces += [(i, j)]
         selected_space = random.choice(blank_spaces)
-        self.spaces[selected_space[0]][selected_space[1]] = random.choices([2, 4], weights=[0.9, 0.1])[0]
+
+        # set space to one of the 
+        self.spaces[selected_space[0]][selected_space[1]] = random.choices([BASE_NUMBER, BASE_NUMBER * 2], weights=[0.9, 0.1])[0]
 
     def has_moves(self) -> bool:
-        for i in range(4):
-            for j in range(4):
+        """
+        Determines whether another move is possible.
+        """
+        for i in range(GRID_SIZE):
+            for j in range(GRID_SIZE):
                 if self.spaces[i][j] is None:
                     return True
                 try:
@@ -89,10 +120,14 @@ class GameBoard():
         return None
 
     def left(self) -> bool:
+        """
+        Attempts to move tiles to the left. Returns a boolean indicating whether
+        any tiles were moved or combined.
+        """
         changed = False
-        board_copy = [[None] * 4 for i in range(4)]
-        for col in range(4):
-            for row in range(4):
+        board_copy = [[None] * GRID_SIZE for i in range(GRID_SIZE)]
+        for col in range(GRID_SIZE):
+            for row in range(GRID_SIZE):
                 if self.spaces[row][col] is None:
                     continue
                 if col == 0:
@@ -111,8 +146,8 @@ class GameBoard():
                     board_copy[row][first_left[1] + 1] = self.spaces[row][col]
                 else:
                     board_copy[row][col] = self.spaces[row][col]
-        for row in range(4):
-            for col in range(4):
+        for row in range(GRID_SIZE):
+            for col in range(GRID_SIZE):
                 if board_copy[row][col] is None:
                     self.spaces[row][col] = None
                 else:
@@ -125,25 +160,29 @@ class GameBoard():
             and the column index of that cell.
             Returns None if no cell found.
         """
-        for i in range(col+1, 4):
+        for i in range(col+1, GRID_SIZE):
             if board_copy[row][i] is not None:
                 return (board_copy[row][i], i)
         return None
 
     def right(self) -> bool:
+        """
+        Attempts to move tiles to the right. Returns a boolean indicating whether
+        any tiles were moved or combined.
+        """
         changed = False
-        board_copy = [[None] * 4 for i in range(4)]
-        for col in range(3, -1, -1):
-            for row in range(4):
+        board_copy = [[None] * GRID_SIZE for i in range(GRID_SIZE)]
+        for col in range(GRID_SIZE - 1, -1, -1):
+            for row in range(GRID_SIZE):
                 if self.spaces[row][col] is None:
                     continue
-                if col == 3:
+                if col == GRID_SIZE - 1:
                     board_copy[row][col] = self.spaces[row][col]
                     continue
                 first_right = self.find_first_right(board_copy, row, col)
                 if first_right is None:
                     changed = True
-                    board_copy[row][3] = self.spaces[row][col]
+                    board_copy[row][GRID_SIZE - 1] = self.spaces[row][col]
                 elif first_right[0] == self.spaces[row][col]:
                     changed = True
                     board_copy[row][first_right[1]] *= -2
@@ -153,8 +192,8 @@ class GameBoard():
                     board_copy[row][first_right[1] - 1] = self.spaces[row][col]
                 else:
                     board_copy[row][col] = self.spaces[row][col]
-        for row in range(4):
-            for col in range(4):
+        for row in range(GRID_SIZE):
+            for col in range(GRID_SIZE):
                 if board_copy[row][col] is None:
                     self.spaces[row][col] = None
                 else:
@@ -173,10 +212,14 @@ class GameBoard():
         return None
 
     def up(self) -> bool:
+        """
+        Attempts to move tiles up. Returns a boolean indicating whether
+        any tiles were moved or combined.
+        """
         changed = False
-        board_copy = [[None] * 4 for i in range(4)]
-        for row in range(4):
-            for col in range(4):
+        board_copy = [[None] * GRID_SIZE for i in range(GRID_SIZE)]
+        for row in range(GRID_SIZE):
+            for col in range(GRID_SIZE):
                 if self.spaces[row][col] is None:
                     continue
                 if row == 0:
@@ -195,8 +238,8 @@ class GameBoard():
                     board_copy[first_up[1] + 1][col] = self.spaces[row][col]
                 else:
                     board_copy[row][col] = self.spaces[row][col]
-        for row in range(4):
-            for col in range(4):
+        for row in range(GRID_SIZE):
+            for col in range(GRID_SIZE):
                 if board_copy[row][col] is None:
                     self.spaces[row][col] = None
                 else:
@@ -205,40 +248,59 @@ class GameBoard():
 
     def find_first_down(self, board_copy, row, col) -> tuple[int, int]:
         """
-        Returns a tuple of the value of the first non-empty cell above the given one,
+        Returns a tuple of the value of the first non-empty cell below the given one,
             and the row index of that cell.
             Returns None if no cell found.
         """
-        for i in range(row+1, 4):
+        for i in range(row+1, GRID_SIZE):
             if board_copy[i][col] is not None:
                 return (board_copy[i][col], i)
         return None
 
     def down(self) -> bool:
+        """
+        Attempts to move tiles down. Returns a boolean indicating whether
+        any tiles were moved or combined.
+        """
         changed = False
-        board_copy = [[None] * 4 for i in range(4)]
-        for row in range(3, -1, -1):
-            for col in range(4):
+        board_copy = [[None] * GRID_SIZE for i in range(GRID_SIZE)]
+        
+        # iterate through rows in reverse order
+        for row in range(GRID_SIZE - 1, -1, -1):
+            for col in range(GRID_SIZE):
+                # this cell is blank
                 if self.spaces[row][col] is None:
                     continue
-                if row == 3:
+
+                # this cell is in the bottom row
+                if row == GRID_SIZE - 1:
                     board_copy[row][col] = self.spaces[row][col]
                     continue
+
+                # find first non-empty cell below this one
                 first_down = self.find_first_down(board_copy, row, col)
+
+                # all cells below are empty
                 if first_down is None:
                     changed = True
-                    board_copy[3][col] = self.spaces[row][col]
+                    board_copy[GRID_SIZE - 1][col] = self.spaces[row][col]
+                
+                # this cell matches the cell below
                 elif first_down[0] == self.spaces[row][col]:
                     changed = True
                     board_copy[first_down[1]][col] *= -2
                     self.score += self.spaces[row][col] * 2
+
+                # the cell below is empty
                 elif first_down[1] > row + 1:
                     changed = True
                     board_copy[first_down[1] - 1][col] = self.spaces[row][col]
+                
+                # the cell below is adjacent and does not match
                 else:
                     board_copy[row][col] = self.spaces[row][col]
-        for row in range(4):
-            for col in range(4):
+        for row in range(GRID_SIZE):
+            for col in range(GRID_SIZE):
                 if board_copy[row][col] is None:
                     self.spaces[row][col] = None
                 else:
@@ -246,18 +308,27 @@ class GameBoard():
         return changed
 
 def handle(c, board) -> None:
+    # esc or q
     if c == '\x1b' or c == 'q':
         print('Exiting...')
         sys.exit(0)
+    
+    # up or w
     elif c == '\x1b[A' or c == 'w':
         if board.up():
             board.add_random_tile()
+
+    # down or s
     elif c == '\x1b[B' or c == 's':
         if board.down():
             board.add_random_tile()
+
+    # right or d
     elif c == '\x1b[C' or c == 'd':
         if board.right():
             board.add_random_tile()
+
+    # left or a
     elif c == '\x1b[D' or c == 'a':
         if board.left():
             board.add_random_tile()
